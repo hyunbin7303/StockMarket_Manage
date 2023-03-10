dcac0cfd0460a033b6c18a4794723a5a07f205c3
kevin <hyunbin7303@gmail.com>
2023-03-10 13:34:33 -0500

    Update for Restful API

--- StockApi/app.py
+++ StockApi/app.py
@@ -1,28 +1,9 @@
 import uuid
 from flask import Flask,request
 from flask_smorest import abort
-from db import stockInfo, stocks
-
+from db import stockNews, stocks
 
 app = Flask(__name__) 
-
-
-
-
-
-
-# [
-#     {
-#         "ticker" : "MSFT",
-#         "company_name" : "Microsoft",
-#         "items": [
-#             {
-#                 "name": "Chair",
-#                 "price": 20
-#             }
-#         ]
-#     }
-# ] 
 
 # @ is python decorator 
 @app.get("/stocks")
@@ -38,49 +19,86 @@ def get_stocks_by_ticker(ticker):
     except KeyError:
          return abort(404, message="Stock not found")
  
-@app.get("/stocks/ById/<string:stock_id>")
+@app.get("/stocks/ById/<int:stock_id>")
 def get_stocks_by_id(stock_id):
     try:
         return stocks[stock_id]
     except KeyError:
          return abort(404, message="Stock not found")
 
-# @app.get("/stocks/<string:ticker>/item")
-# def get_stocks_by_ticker(ticker):
-#     for stock in stocks:
-#         if stock["ticker"] == ticker:
-#             return {"items" : stock["items"]} 
-#     return {"message": "Stock cannot be found"}, 404
-
-
 @app.post("/stocks")
 def create_new_stock():
     stock_data = request.get_json()
     stock_id = uuid.uuid4().hex
     new_stock = { **stock_data, "id": stock_id }
+    
+    for stock in stocks.values():
+        if stock_data["ticker"] == stock["ticker"]:
+            abort(400, message=f"Stock ticker already exist.")
+
     stocks[stock_id] = new_stock 
     return new_stock, 201
     
-@app.post("/stockinfo")
-def create_info():
-    info_data = request.get_json()
-    if info_data["stock_id"] not in stocks:
-         return abort(404, message="Stock not found")
-     
-    info_id = uuid.uuid4().hex
-    info = {**info_data, "id": info_id}
-    stockInfo[info_id] = info  
+@app.put("/stocks/<int:stock_id>")
+def update_stock(stock_id):
+    stock_data = request.get_json()
+    if "symbol" not in stock_data or "company_name" not in stock_data:
+        abort(400, message="Bad Request. Ensure Symbol or company name are included in the json payload.")
 
-    return info, 201
-
-@app.get("/stockinfos")
-def get_all_infos():
-    return {"infos" : list(stockInfo.values())}
-
-
-@app.get("/stockinfo/<string:stockinfo_id>")
-def get_stockinfo_byId(stockinfo_id):
     try:
-        return stockInfo[stockinfo_id]
+        stock = stocks[stock_id]
+        stock |= stock_data
+        return stock 
+    except KeyError:
+        abort(404, message="Stock not found")
+
+@app.get("/stocknews")
+def get_all_news():
+    return {"news" : list(stockNews.values())}
+
+@app.get("/stocknews/<string:stocknews_id>")
+def get_stockNews_byId(stockinfo_id):
+    try:
+        return stockNews[stockinfo_id]
     except KeyError:
         return abort(404, message="Stock not found")
+
+@app.post("/stocknews")
+def create_stock_news():
+    news_data = request.get_json()
+
+    if "ticker" not in news_data or "title" not in news_data:
+        abort(400, message= "Bad request. Ensure 'ticker' and 'title' are included in the Json payload. ")
+    
+
+    # for new in stockNews.values():
+        # if news_data[""]
+
+    if news_data["stock_id"] not in stocks:
+        return abort(404, message="Stock not found")
+     
+    news_id = uuid.uuid4().hex
+    news = {**news_data, "id": news_id}
+    stockNews[news_id] = news  
+
+    return news, 201
+
+@app.post("/stocknews/<string:news_id>")
+def update_stocknews(news_id):
+    news_data = request.get_json()
+    if "ticker" not in news_data or "title" not in news_data:
+        abort(400, message="Bad request. Ensure ticker and title are included in the json payload.")
+
+    try:
+        news = stockNews[news_id]
+        news |= news_data
+    except KeyError:
+        abort(404, message="news not found")
+
+@app.delete("/stocknews/<string:news_id>")
+def delete_stocknews(news_id):
+    try:
+        del stockNews[news_id]
+        return {"message" : "news deleted"}
+    except KeyError:
+        abort(404, message="news not found")
--- StockApi/db.py
+++ StockApi/db.py
@@ -1,11 +1,17 @@
 stocks = {}
-stockInfo = {
+stockNews = {
     1: {
         "ticker" : "MSFT",
-        "Title" : "MSFT info"
+        "Title" : "MSFT info",
+        "Description" : ""
     },
     2: {
         "ticker":"TSLA",
-        "Title": "TSLA NEWS"
+        "Title": "TSLA NEWS",
+        "Description" : "Why it goes down..."
     }
 } 
+
+
+# Create Common folder for sharing Db Access or others etc....
+# CHeck other solutions how they solved this issue. 
