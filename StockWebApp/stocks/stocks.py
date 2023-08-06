1e276d813a51470b31b08fa485649f058e6d8b0e
kevin <hyunbin7303@gmail.com>
2023-08-06 15:50:38 -0400

    Inserting stock using post request

--- StockWebApp/stocks/stocks.py
+++ StockWebApp/stocks/stocks.py
@@ -1,5 +1,5 @@
 import uuid
-from flask import request, jsonify
+from flask import request, jsonify, make_response, Response
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
 from dbaccess import DbConnection, Database
@@ -7,6 +7,7 @@ from db_session import database_instance
 from models import Stock
 from schemas import StockSchema, StockUpdateSchema,StockFinancialSchema
 from psycopg.rows import dict_row
+from werkzeug.exceptions import HTTPException
 
 
 
@@ -15,6 +16,7 @@ stocks_bp = Blueprint("stocks", __name__, description="Operations on Stocks")
 @stocks_bp.route('/stocks', methods=['GET'])
 @stocks_bp.response(200, StockSchema(many=True))
 def get_stocks(): 
+
     try:
         with database_instance.get_connection() as conn:
             cur = conn.cursor(row_factory=dict_row)
@@ -51,10 +53,8 @@ def get_stockbyId(stock_id: int):
 def get_stock_by_ticker(ticker: str): 
     try:
         with database_instance.get_connection() as conn:
-            conn.execute("SELECT * FROM stocks where ticker = {};", {""})
+            conn.execute("SELECT * FROM stocks where ticker = %{ticker}s;", {ticker: ticker})
             cur = conn.cursor()
-            # cur.execute("""select * from stocks where ticker = %(ticker)s """, {"stock_id": stock_id})
-            rows = cur.fetchall()
         pass # need to be updated after figuring out Stock endpoint practice.
     except KeyError:
         abort(404, message="Ticker cannot be found.")
@@ -65,19 +65,6 @@ def get_stock_by_ticker(ticker: str):
 
 @stocks_bp.route('/stocks', methods=['POST'])
 def post():
-    # try:
-    #     with database_instance.get_connection() as conn:
-    #         cur = conn.cursor()
-    #         result = cur.execute("SELECT * FROM stocks;").fetchall()
-    # except KeyError:
-    #     abort(404, message="Ticker cannot be found.")
-
-    # for stock in result:
-    #     if stock_data["ticker"] == stock["ticker"] or stock:
-    #         abort(400, message=f"Stock ticker already exist.")
-
-
-    # insert_query = 'insert', """INSERT INTO Stocks(ticker, company_name, stock_desc, stock_type, stock_sector,stock_exchange) VALUES (%s, %s, %s, %s, %s, %s);""",(stock_data['ticker'], stock_data['company_name'], stock_data['stock_desc'],stock_data['stock_type'],stock_data['stock_sector'],stock_data['stock_exchange'])
     try: 
         ticker =        request.json['ticker']
         company_name =  request.json['company_name']
@@ -85,24 +72,38 @@ def post():
         stock_type =    request.json['stock_type']
         stock_sector =  request.json['stock_sector']
         stock_exchange = request.json['stock_exchange']
-        new_stock = Stock(ticker, company_name, stock_desc, stock_type, stock_sector, stock_exchange)
+        # new_stock = Stock(ticker, company_name, stock_desc, stock_type, stock_sector, stock_exchange)
     except KeyError:
         abort(404, message= "Cannot insert the new stock")
     
     try:
         with database_instance.get_connection() as conn:
-            cur = conn.cursor(row_factory=dict_row)
+            cur = conn.cursor()
+            cur.execute("""select * from stocks where ticker = %(ticker)s """, {"ticker": ticker})
+            rows = cur.fetchone()
+            if rows:
+                raise KeyError("Already Exist Tickers = ", ticker)
 
-            # result = cur.execute("SELECT * FROM stocks;").fetchall()
+            cur.execute("""INSERT INTO stocks(ticker, company_name, stock_desc, stock_type, stock_sector,stock_exchange) VALUES (%(ticker)s, %(company_name)s, %(stock_desc)s, %(stock_type)s, %(stock_sector)s, %(stock_exchange)s)""", 
+                        {"ticker": ticker, "company_name": company_name, "stock_desc":stock_desc, "stock_type": stock_type, "stock_sector": stock_sector, "stock_exchange":stock_exchange})
 
-    except KeyError:
-        abort(404, message="Ticker cannot be found.")
+            conn.commit()
+            cur.close()
+
+    except KeyError as err:
+        return Response(err.args, status=403)
+    except ValueError as err:
+        return Response(err.args, status =404)
+
     finally:
-        cur.close()
         database_instance.return_connection(conn)
 
-    return result
-
+    data={
+        "StatusCode": 200, 
+        "Subject": "Successful"
+    }
+    return Response({}, status=201)
+    
 
 @stocks_bp.response(200, StockSchema)
 def delete(self, stock_id):
@@ -120,3 +121,5 @@ def delete(self, stock_id):
 #         # but do we need to do this if we are using the foreign key?
 #         # Let's figure it out.
 #         pass
+def custom_error(message, status_code): 
+    return make_response(jsonify(message), status_code)
