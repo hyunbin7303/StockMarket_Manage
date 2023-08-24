f2fef3cadbce4bdd141e327892778da9dea5711b
Zio4 <64350954+Zio4@users.noreply.github.com>
2023-08-25 00:53:47 +0900

    Update stocknews.py
    error clear

--- StockWebApp/stocknews/stocknews.py
+++ StockWebApp/stocknews/stocknews.py
@@ -1,6 +1,6 @@
 import uuid
 import string
-from flask import request
+from flask import request, jsonify, make_response, Response
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
 from schemas import StockNewsSchema
@@ -49,12 +49,11 @@ def put_stocknews(stocknews_id):
     except KeyError:
         abort(404, message="news not found")
          
-@stocknews_bp.route('/stocksnews', methods=['POST'])
+@stocknews_bp.route('/stocknews', methods=['POST'])
 def post():
     try: 
-        news_id = request.json['news_id']
+        
         title =  request.json['title']
-#        price_before =    request.json['price_before']
         cause =    request.json['cause']
         impact_on_stock = request.json['impact_on_stock']
         price_before = request.json['price_before']
@@ -65,13 +64,13 @@ def post():
     try:
         with database_instance.get_connection() as conn:
             cur = conn.cursor()
-            cur.execute("""select * from stocksnews where title = %(title)s """, {"title": title})
+            cur.execute("""select * from stocknews where title = %(title)s """, {"title": title})
             rows = cur.fetchone()
             if rows:
                 raise KeyError("Already Exist title = ", title)
 
-            cur.execute("""INSERT INTO stocksnews(news_id, title, price_before, cause, impact_on_stock) VALUES (%(news_id)s, %(title)s, %(price_before)s, %(cause)s, %(impact_on_stock)s)""", 
-                        {"news_id": news_id, "title": title, "price_before": price_before, "cause": cause, "impact_on_stock": impact_on_stock})
+            cur.execute("""INSERT INTO stocknews(title, price_before, cause, impact_on_stock) VALUES (%(title)s, %(price_before)s, %(cause)s, %(impact_on_stock)s)""", 
+                        {"title": title, "price_before": price_before, "cause": cause, "impact_on_stock": impact_on_stock})
 
             conn.commit()
             cur.close()
